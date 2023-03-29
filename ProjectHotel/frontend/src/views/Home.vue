bd37915258cf1d07a8e7f236677aa35fed500ab2
PUTTIPONG3035 <64070078@kmitl.ac.th>
2023-03-29 10:16:14 +0700

    Ep1

---
+++ ProjectHotel/frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div >
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <h1>Folk</h1>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
